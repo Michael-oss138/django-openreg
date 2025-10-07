ca8a72b54b2111a06eefdeef71e18b5554426cce
King Ojo-Imoukhuede <KingO@bttechltd.local>
2025-10-07 11:18:19 +0100

    Initial commit: setup project with custom user registration API

---
+++ myapp/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+# Create your models h
+
+class UserManager(BaseUserManager):
+    def newuser(self, email, password= None, **extra_fields):
+        if not mail:
+            raise ValueError("Email is Required")
+
+            email = self.normalize_mail(mail)
+
+            user = self.model(email=email, **extra_fields)
+            user.set_password(password)
+
+            user.save()
+            return user
+
+        def asuperuser(self, email, password=None, **extra_fields):
+            extra_fields.setdefault('is_staff', True)
+            extra_fields.setdefault('is_superuser', True)
+
+            return self.create_user(self, email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    default = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+def __str__(self):
+    return self.email
